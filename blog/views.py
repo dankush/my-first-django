1725c0413eb8ed29f723abbfbd8969356ef9da4f
danku <dan.k0288@gmail.com>
2016-10-23 23:55:07 +0300

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -12,6 +12,3 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-
-
-Post.objects.get(pk=pk)
